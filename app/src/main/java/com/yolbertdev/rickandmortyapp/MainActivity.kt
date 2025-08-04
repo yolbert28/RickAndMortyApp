5f53f11df5413230a0ae2681bc0751c9c3e4ada0
yolbert28 <yolbertdev@gmail.com>
2025-08-04 11:01:06 -0400

    agregando splashScreen y logo de la app

--- app/src/main/java/com/yolbertdev/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/yolbertdev/rickandmortyapp/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yolbertdev.rickandmortyapp.ui.theme.RickAndMortyAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
